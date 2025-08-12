783286810589fc4d76f2f1624509eeffc19ebbe8
Yaswant <yaswantsoni2005@gmail.com>
2025-08-12 15:58:27 +0530

    minor bug fixed

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -3,6 +3,7 @@ import {DB_NAME} from "../constants/db.js"
 
 export const connectDB = async () => {
   try {
+    console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log("\nMONGODB CONNECTED! HOST: ",connectionInstance.connection.host);
   } catch (error) {
