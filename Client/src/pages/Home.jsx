f89561e3acc11724d399d7070481d169ee6085b3
Yaswant <yaswantsoni2005@gmail.com>
2025-09-12 02:02:38 +0530

    Fixed: minor bug fixed

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  useEffect(() => {
+    const navigation = useNavigate();
+  navigation("/login");
+  }, []);
   return (
     <div>
       Home Page
